ad7313361997f16d7d91ad69fbf74e88443f5916
Auspicious14 <uthmanabdulganiyu2019@gmail.com>
2024-08-19 22:42:16 +0100

    [update] about, projects page

---
+++ src/modules/about/components/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import SkillPattern from "./../../../../public/Group 36.png";
+export const AboutComponent = () => {
+  return (
+    <div>
+      <div className="lg:flex md:block gap-12 justify-between items-center">
+        <div className="text-secondary lg:w-1/2">
+          <p className="my-4 text-xl text-justify">Hello, I'm Auspicious!</p>
+          <p>
+            I'm a self-taught software developer based in Ilorin, Nigeria. I can
+            develop responsive websites from scratch and raise them into modern
+            user-friendly web experiences.
+          </p>
+          <p className="my-3 text-justify">
+            Transforming my creativity and knowledge into a websites has been my
+            passion for over a year. I have been helping various clients to
+            establish their presence online. I always strive to learn about the
+            newest technologies and frameworks.
+          </p>
+        </div>
+        <div className="lg:w-1/2 flex justify-center lg:block">
+          <Image
+            src={SkillPattern}
+            alt="vendify"
+            width={400}
+            height={400}
+            className="object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/modules/projects/components/index.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import VendifyLogo from "./../../../../public/vendify logo white.jpg";
+import CareerGuideIMage from "./../../../../public/career-guidance.png";
+
+export const ProjectComponent = () => {
+  return (
+    <section className="">
+      <div className="md:flex justify-center gap-8 ">
+        <div className="w-full border-2 rounded-lg border-dashed my-4 md:my-0 border-secondary">
+          <Image
+            src={VendifyLogo}
+            alt="vendify"
+            width={500}
+            height={500}
+            className="bg-primary w-full rounded-t-lg object-cover"
+          />
+          <div className="border border-b p-4">
+            Typescript, Next.js, Node.js
+          </div>
+          <div className="p-4">
+            <h1 className="text-3xl font-semibold text-white mb-2">Vendify</h1>
+            <p>
+              A multi-vendor E-commerce platform designed to ease both vendor
+              and customer shopping experience.
+            </p>
+            <Link
+              href="https://vendiify.vercel.app"
+              className="my-8 text-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check out
+            </Link>
+          </div>
+        </div>
+        <div className="w-full border-2 rounded-lg border-dashed border-secondary">
+          <Image
+            src={CareerGuideIMage}
+            alt="career-guidance"
+            width={500}
+            height={500}
+            className="bg-primary w-full rounded-t-lg object-cover"
+          />
+          <div className="border border-b border-secondary p-4">
+            Typescript, Next.js, Node.js
+          </div>
+          <div className="p-4">
+            <h1 className="text-3xl font-semibold text-white mb-2">
+              Career Guidance
+            </h1>
+            <p>A platform to help people navigate and choose their careers.</p>
+
+            <Link
+              href="https://career-guidance.com.ng"
+              className="text-blue-600 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check out
+            </Link>
+          </div>
+        </div>
+        <div className="w-full border-2 rounded-lg border-dashed my-4 md:my-0 border-secondary">
+          <Image
+            src={VendifyLogo}
+            alt="vendify"
+            width={500}
+            height={500}
+            className="bg-primary w-full rounded-t-lg object-cover"
+          />
+          <div className="border border-b border-secondary p-4">
+            Typescript, Next.js, Node.js
+          </div>
+          <div className="p-4">
+            <h1 className="text-3xl font-semibold text-white mb-2">Cv-Craft</h1>
+            <p>
+              A platform to help job seekers craft professional and beautiful
+              CVs.
+            </p>
+
+            <Link
+              href="https://cv-craft.netlify.app"
+              className="text-blue-600 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check out
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
